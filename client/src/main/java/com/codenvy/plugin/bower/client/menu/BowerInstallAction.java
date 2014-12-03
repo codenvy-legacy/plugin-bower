21f193217a14974d19a8826fad470f6620090d06
Alexander Reshetnyak <areshetnyak@codenvy.com>
2014-12-03 10:28:55 +0200

    DASHB-586: Move AnalyticsEventLogger to client package.

--- client/src/main/java/com/codenvy/plugin/bower/client/menu/BowerInstallAction.java
+++ client/src/main/java/com/codenvy/plugin/bower/client/menu/BowerInstallAction.java
@@ -10,7 +10,7 @@
  *******************************************************************************/
 package com.codenvy.plugin.bower.client.menu;
 
-import com.codenvy.api.analytics.logger.AnalyticsEventLogger;
+import com.codenvy.api.analytics.client.logger.AnalyticsEventLogger;
 import com.codenvy.api.builder.BuildStatus;
 import com.codenvy.api.builder.dto.BuildOptions;
 import com.codenvy.ide.api.action.ActionEvent;
